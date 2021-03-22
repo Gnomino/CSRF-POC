c482d6b6cee27b1412dcb93a1e5d156d0f4c12d4
Buresi Thomas <thomas.buresi@insa-lyon.fr>
2021-03-23 00:00:04 +0100

    Added style to Forum

--- server-side/forum.php
+++ server-side/forum.php
@@ -6,36 +6,41 @@ if(!isset($user)) { // If the user is not logged in, redirect them to the welcom
     exit();
 }
 ?>
-<!doctype html>
-<html>
-<head>
+<!DOCTYPE html>
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <title>Welcome</title>
+    <link href="./styles/forum.css" rel="stylesheet">
     <meta charset="utf-8">
 </head>
 <body>
-    <h1>Forum</h1>
-    <hr>
-    <?php
-        $posts = Model::factory('Post')->find_many();
-        if(empty($posts)) {
-            echo 'Nothing has been posted yet.';
-        } else {
-            foreach($posts as $post) {
-                echo '<div id="post' . $post->id . '">';
-                echo 'By ' . htmlspecialchars($post->user()->find_one()->username) . ' on ' . $post->creation_time . '<br>';
-                echo '<blockquote>' . nl2br(htmlspecialchars($post->contents)) . '</blockquote>';
-                echo '</div><hr>';
-            }
-        }
-    ?>
-    <h3>Write a post</h3>
-    <form action="post.php" method="post">
-        <label for="post">
-            Text
-        </label>
-        <textarea id="post" name="post" required></textarea>
+    <div id="head">
+        <h1 id="title">Forum</h1>
+        <form action="logout.php" method="post" >
+            <button id="buttonLog" type="submit">Logout !</button>
+        </form>
+    </div>
+    <hr class="striped-border">
         <br>
-        <button type="submit">Post !</button>
+        <div id="postsFeed">
+            <?php
+            $posts = Model::factory('Post')->find_many();
+            if(empty($posts)) {
+                echo 'Nothing has been posted yet.';
+            } else {
+                foreach($posts as $post) {
+                    echo '<div id="post' . $post->id . '">';
+                    echo 'By ' . htmlspecialchars($post->user()->find_one()->username) . ' on ' . $post->creation_time . '<br>';
+                    echo '<blockquote>' . nl2br(htmlspecialchars($post->contents)) . '<hr></blockquote>';
+                    echo '</div>';
+                }
+            }
+        ?></div>   <h3 class="write">Write a post</h3>
+    <form action="post.php" method="post">
+        <textarea id="post" style="resize: none; height: 100px; width: 300px;" name="post" placeholder= "Entrez votre post ici "required=""></textarea>
+        <br>
+        <button class="buttonPost" type="submit"><span>Post !</span> </button>
     </form>
-</body>
-</html>
+ 
+</body></html>
+    
